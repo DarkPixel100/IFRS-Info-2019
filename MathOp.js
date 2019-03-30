c8eb32fd9e3ae634d852cc7f08469381525cfac0
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-03-29 22:47:03 -0300

    Add files via upload

---
+++ MathOp.js
@@ -0,0 +1,11 @@
+Math.PI; //Valor de PI 
+Math.E; //Valor de E
+Math.pow(x,y); //Potência
+Math.sqrt(x); //Ao quadrado
+Math.abs(x); //Pega o valor cru e sem sinal
+Math.round(x.?); //Arredonda
+Math.ceil(x.?); //Arredonda para cima
+Math.floor(x.?); //Arredonda para baixo (Mesma coisa que o parseInt)
+Math.min(x,y,z...); //Valor mínimo dentre os valores colocados 
+Math.max(x,y,z...); //Valor máximo dentre os valores colocados
+Math.random();//Valor aleatório entre de 0.000...1 à 0.999...
