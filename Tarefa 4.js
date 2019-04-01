f0059d31f01f959f3d6c2d36803d351756efabc9
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-04-01 15:42:55 -0300

    Create Tarefa 4.js

---
+++ Tarefa 4.js
@@ -0,0 +1,20 @@
+var a,b,c,d,e,f,g,num;
+scanf('%d','num');
+scanf('%d','a');
+scanf('%d','b');
+scanf('%d','c');
+scanf('%d','d');
+scanf('%d','e');
+scanf('%d','f');
+scanf('%d','g');
+var sumall = a + b + c + g - d - e - f;
+if(sumall != num)
+{
+   printf('S');
+
+}
+else
+{
+   printf('N');
+
+}
