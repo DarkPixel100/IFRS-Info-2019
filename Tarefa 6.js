9cce7ce2fe3cf0be6c5fc79e672fb9ef999fa06d
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-04-08 16:50:27 -0300

    Create Tarefa 6.js

---
+++ Tarefa 6.js
@@ -0,0 +1,26 @@
+var caps, nCoin;
+var cicles = [];
+var auxCoin = 0;
+
+scanf('%d','caps');
+scanf('%d','nCoin');
+
+for(var i=0;i<caps;i++)
+{
+	scanf('%d','cicles[i]');
+}
+for(days=1;days<=Math.pow(10,8);days++)
+{
+	if(auxCoin >= nCoin)
+    {
+       printf('%d',days-1);
+       break;
+    }
+    for(j=0;j<caps;j++)
+    {
+    	if(days%cicles[j]==0)
+        {
+           auxCoin++;
+        }
+    }
+}
