2fd56e1a776b5130049f79383be25a13f86254b2
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-05-09 22:57:23 -0300

    Update TRABALHO_WIP_0905.js

--- TRABALHO_WIP_0905.js
+++ TRABALHO_WIP_0905.js
@@ -23,4 +23,26 @@ alert("A metade do intervalo das duas horas é de "+hR+":"+mR+":"+sR+".");
 var data, dia, mes, ano;
 data = parseInt(prompt("Digite a data no formato DDMMAA:"));
 dia = parseInt(data/10000);
-mes = ((data/100) - (Math.floor(data/100))) * 
+mes = parseInt(((data/10000) - (Math.floor(data/10000))) * 100);
+ano = parseInt(((data/100) - (Math.floor(data/100))) * 100);
+alert("A data no formato AA/MM/DD é "+ano+"/"+mes+"/"+dia+".");
+//Exercício 4:
+var n, mult;
+n = parseInt(prompt("Digite um número:"));
+mult = (n - n%5) + 5;
+alert("O primeiro número primo posterior ao número digitado é "+mult+".");
+//Exercício 5:
+var a, b, c, delta, x1, x2;
+a = Number(prompt("Digite o a(número que multipica o x²)"));
+b = Number(prompt("Digite o b(número que multipica o x)"));
+c = Number(prompt("Digite o c(número que não multiplica nenhum x(se não aparecer na equação, digite 0))"));
+delta = (Math.pow(b,2) - 4 * a * c);
+x1 = ((-b)+Math.sqrt(delta))/(2*a);
+x2 = ((-b)-Math.sqrt(delta))/(2*a);
+alert("A primeira raíz é "+x1+" e a segunda é "+x2+".");
+//Exercício 6:
+var t, outros15, fPrice;
+t = parseInt(prompt("Digite o tempo de permanência(em minutos):"));
+outros15 = Math.ceil((t-30)/15);
+fPrice = 20 + (10*outros15);
+alert("O preço final será de R$"+fPrice.toFixed(2)+".");
