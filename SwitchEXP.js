4d4024226bb26bc55da8eab505378126d7c4e016
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-05-23 14:59:38 -0300

    Create SwitchEXP.js

---
+++ SwitchEXP.js
@@ -0,0 +1,13 @@
+var variavel;
+var n = prompt();//Digite algo para ser colocado em n
+switch(variavel)
+{
+  case n:
+    comandos;
+    break;
+  case n:
+    comandos;
+      break;
+  default:
+  comandos;
+}
