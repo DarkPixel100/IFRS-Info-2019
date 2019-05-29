a9aa12d922e85380045846bf4ee76e2437dd5d5d
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-05-29 15:56:34 -0300

    Create MorangosOBI2016.js

---
+++ MorangosOBI2016.js
@@ -0,0 +1,9 @@
+var l1, c1, l2, c2, a1, a2, am;
+scanf("%d", 'l1');
+scanf("%d", 'c1');
+scanf("%d", 'l2');
+scanf("%d", 'c2');
+a1 = l1 * c1;
+a2 = l2 * c2;
+am = Math.max(a1,a2);
+printf("%d",am);
