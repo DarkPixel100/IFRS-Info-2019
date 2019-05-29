c3ab7e2c9820ba1f7fd988bf1dc5f8ec16587ecd
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-05-29 15:57:04 -0300

    Create zipOBI2017f1.js

---
+++ zipOBI2017f1.js
@@ -0,0 +1,43 @@
+var l1, l2, c1, c2;
+var lia = 0;
+var carol = 0;
+scanf("%d","l1");
+scanf("%d","l2");
+scanf("%d","c1");
+scanf("%d","c2");
+if(l1 == l2)
+{
+    lia = (l1+l2)*2;
+}
+if((l1+1 == l2) || (l1-1 == l2))
+{
+    lia = (l1+l2)*3;
+}
+else
+{
+    lia = l1+l2;
+}
+if(c1 == c2)
+{
+    carol = (c1+c2)*2;
+}
+if((c1+1 == c2) || (c1-1 == c2))
+{
+    carol = (c1+c2)*3;
+}
+else
+{
+    carol = c1+c2;
+}
+if(lia > carol)
+{
+    printf("Lia");
+}
+if(carol > lia)
+{
+    printf("Carolina");
+}
+else
+{
+    printf("Empate");
+}
