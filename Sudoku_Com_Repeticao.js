a4df9f49e8fc29b330f64cd128981a4572dba879
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-06-14 15:00:38 -0300

    Update Sudoku_Com_Repeticao.js

--- Sudoku_Com_Repeticao.js
+++ Sudoku_Com_Repeticao.js
@@ -1,4 +1,4 @@
-var num, coord, jogo, aux, sc;
+var coord, jogo, aux, jn;
 var Vjogo = [];
 var VjogoLCQ = [];
 alert("Esse é um sudoku, as regras são:\n1-Esse é um tabuleiro 4x4, ou seja, o números a serem inseridos devem ser entre 1 e 4;\n2-Não podemexistir números iguais em uma mesma linha, coluna ou quadrante(Quadrados 2x2 que dividem o tabuleiro em 4);\n3-Um número inserido não pode ser substituído.\nDivirta-se!");
@@ -15,14 +15,13 @@ function Game()
     aux = 0;
     function Jogada()
     {
-        var jogo = "  1 2 3 4\nA|"+Vjogo[0]+"|"+Vjogo[1]+"|"+Vjogo[2]+"|"+Vjogo[3]+"|\nB|"+Vjogo[4]+"|"+Vjogo[13]+"|"+Vjogo[5]+"|"+Vjogo[6]+"|\nC|"+Vjogo[14]+"|"+Vjogo[7]+"|"+Vjogo[8]+"|"+Vjogo[9]+"|\nD|"+Vjogo[10]+"|"+Vjogo[11]+"|"+Vjogo[15]+"|"+Vjogo[12]+"|";
-        var coord = prompt("Digite a coordenada onde o número será colocado:\n"+jogo);
-        var num = Number(prompt("Digite o número a ser colocado:\n"+jogo));
+        jogo = "  1 2 3 4\nA|"+Vjogo[0]+"|"+Vjogo[1]+"|"+Vjogo[2]+"|"+Vjogo[3]+"|\nB|"+Vjogo[4]+"|"+Vjogo[13]+"|"+Vjogo[5]+"|"+Vjogo[6]+"|\nC|"+Vjogo[14]+"|"+Vjogo[7]+"|"+Vjogo[8]+"|"+Vjogo[9]+"|\nD|"+Vjogo[10]+"|"+Vjogo[11]+"|"+Vjogo[15]+"|"+Vjogo[12]+"|";
+        coord = prompt("Digite a coordenada onde o número será colocado:\n"+jogo);
         if(coord == 'a1' || coord == 'A1')
         {
             if(Vjogo[0] == '_')
             {
-                Vjogo[0] = num;
+                Vjogo[0] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -34,7 +33,7 @@ function Game()
         {
             if(Vjogo[1] == '_')
             {
-                Vjogo[1] = num;
+                Vjogo[1] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -46,7 +45,7 @@ function Game()
         {
             if(Vjogo[2] == '_')
             {
-                Vjogo[2] = num;
+                Vjogo[2] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -58,7 +57,7 @@ function Game()
         {
             if(Vjogo[3] == '_')
             {
-                Vjogo[3] = num;
+                Vjogo[3] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -70,7 +69,7 @@ function Game()
         {
             if(Vjogo[4] == '_')
             {
-                Vjogo[4] = num;
+                Vjogo[4] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -80,21 +79,14 @@ function Game()
         }
         if(coord == 'b2' || coord == 'B2')
         {
-            if(Vjogo[5] == '_')
-            {
-                Vjogo[5] = num;
-            }
-            else
-            {
                 alert("Essa posição não está disponível, tente novamente.");
                 Jogada();
-            }   
         }
         if(coord == 'b3' || coord == 'B3')
         {
-            if(Vjogo[6] == '_')
+            if(Vjogo[5] == '_')
             {
-                Vjogo[6] = num;
+                Vjogo[5] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -104,9 +96,9 @@ function Game()
         }
         if(coord == 'b4' || coord == 'B4')
         {
-            if(Vjogo[7] == '_')
+            if(Vjogo[6] == '_')
             {
-                Vjogo[7] = num;
+                Vjogo[6] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -116,21 +108,14 @@ function Game()
         }
         if(coord == 'c1' || coord == 'C1')
         {
-            if(Vjogo[8] == '_')
-            {
-                Vjogo[8] = num;
-            }
-            else
-            {
                 alert("Essa posição não está disponível, tente novamente.");
                 Jogada();
-            }   
         }
         if(coord == 'c2' || coord == 'C2')
         {
-            if(Vjogo[9] == '_')
+            if(Vjogo[7] == '_')
             {
-                Vjogo[9] = num;
+                Vjogo[7] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -140,9 +125,9 @@ function Game()
         }
         if(coord == 'c3' || coord == 'C3')
         {
-            if(Vjogo[10] == '_')
+            if(Vjogo[8] == '_')
             {
-                Vjogo[10] = num;
+                Vjogo[8] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -152,9 +137,9 @@ function Game()
         }
         if(coord == 'c4' || coord == 'C4')
         {
-            if(Vjogo[11] == '_')
+            if(Vjogo[9] == '_')
             {
-                Vjogo[11] = num;
+                Vjogo[9] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -164,9 +149,9 @@ function Game()
         }
         if(coord == 'd1' || coord == 'D1')
         {
-            if(Vjogo[12] == '_')
+            if(Vjogo[10] == '_')
             {
-                Vjogo[12] = num;
+                Vjogo[10] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -176,9 +161,9 @@ function Game()
         }
         if(coord == 'd2' || coord == 'D2')
         {
-            if(Vjogo[13] == '_')
+            if(Vjogo[11] == '_')
             {
-                Vjogo[13] = num;
+                Vjogo[11] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -188,21 +173,14 @@ function Game()
         }
         if(coord == 'd3' || coord == 'D3')
         {
-            if(Vjogo[14] == '_')
-            {
-                Vjogo[14] = num;
-            }
-            else
-            {
                 alert("Essa posição não está disponível, tente novamente.");
                 Jogada();
-            }   
         }
         if(coord == 'd4' || coord == 'D4')
         {
-            if(Vjogo[15] == '_')
+            if(Vjogo[12] == '_')
             {
-                Vjogo[15] = num;
+                Vjogo[12] = Number(prompt("Digite o número a ser colocado:\n"+jogo));
             }
             else
             {
@@ -210,7 +188,7 @@ function Game()
                 Jogada();
             }   
         }
-        else if (coord != 'a1' && coord != 'A1' && coord != 'a2' && coord != 'A2' && coord != 'a3' && coord != 'A3' && coord != 'a4' && coord != 'A4' && coord != 'b1' && coord != 'B1' && coord != 'b2' && coord != 'B2' && coord != 'b3' && coord != 'B3' && coord != 'b4' && coord != 'B4' && coord != 'c1' && coord != 'C1' && coord != 'c2' && coord != 'C2' && coord != 'c3' && coord != 'C3' && coord != 'c4' && coord != 'C4' && coord != 'd1' && coord != 'D1' && coord != 'd2' && coord != 'D2' && coord != 'd3' && coord != 'D3' && coord != 'd4' && coord != 'D4')
+        if (coord != 'a1' && coord != 'A1' && coord != 'a2' && coord != 'A2' && coord != 'a3' && coord != 'A3' && coord != 'a4' && coord != 'A4' && coord != 'b1' && coord != 'B1' && coord != 'b2' && coord != 'B2' && coord != 'b3' && coord != 'B3' && coord != 'b4' && coord != 'B4' && coord != 'c1' && coord != 'C1' && coord != 'c2' && coord != 'C2' && coord != 'c3' && coord != 'C3' && coord != 'c4' && coord != 'C4' && coord != 'd1' && coord != 'D1' && coord != 'd2' && coord != 'D2' && coord != 'd3' && coord != 'D3' && coord != 'd4' && coord != 'D4')
         {
             alert("Essa posição não existe.");
         }
@@ -231,23 +209,33 @@ function Game()
     VjogoLCQ[9] = Vjogo[2] + Vjogo[3] + Vjogo[5] + Vjogo[6];
     VjogoLCQ[10] = Vjogo[14] + Vjogo[7] + Vjogo[10] + Vjogo[11];
     VjogoLCQ[11] = Vjogo[8] + Vjogo[9] + Vjogo[15] + Vjogo[12];
-    for(k=0;k<12;k++)
-    if(VjogoLCQ[0] == VjogoLCQ[k])
+    for(k=1;k<12;k++)
     {
-        aux++;
+        if(VjogoLCQ[0] == VjogoLCQ[k])
+        {
+            aux++;
+        }
     }
-    if(aux == 13)
+    if(aux == 11)
     {
-        alert("Parabéns! Você venceu!");
-    }
-    else
-    {
-        sc = prompt("Você perdeu, deseja jogar novamente?(Digite \"Sim\" ou \"Não\")");
-        if (sc == "Não" || sc == "não" || sc == "Nao" || sc == "nao")
+        jn = prompt("Parabéns! Você venceu! Deseja jogar novamente?(Digite \"Sim\" ou \"Não\")");
+        if (jn == "Não" || jn == "não" || jn == "Nao" || jn == "nao")
         {
             alert("Ok, tchau :)");
         }
-        else if(sc == "Sim" || sc == "sim")
+        else if(jn == "Sim" || jn == "sim")
+        {
+            Game();
+        }
+    }
+    else
+    {
+        jn = prompt("Você perdeu, deseja jogar novamente?(Digite \"Sim\" ou \"Não\")");
+        if (jn == "Não" || jn == "não" || jn == "Nao" || jn == "nao")
+        {
+            alert("Ok, tchau :)");
+        }
+        else if(jn == "Sim" || jn == "sim")
         {
             Game();
         }
