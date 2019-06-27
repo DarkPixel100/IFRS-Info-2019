a047423168fff5f8a91bf489b1217c259e157d48
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-06-27 14:42:01 -0300

    Update Exercícios 2706.js

--- Exercícios 2706.js
+++ Exercícios 2706.js
@@ -1,8 +1,10 @@
 //Exercício 1:
 var c = 1;
+var auxS = "1";
 var n = parseInt(prompt("Digite um número natural(Será apresentada uma sequencia de 1² a N²):"));
-while(c<=n)
+while(c<n)
 {
-  alert(c**2);
   c++;
+  auxS += ", "+c*c;
 }
+alert(auxS);
