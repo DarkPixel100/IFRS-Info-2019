de0e0919df2a417dbc2a50c33c2dbc0c63ec74a1
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-07-05 12:11:46 -0300

    Create Exercícios 0507(TEM QUE ENVIAR).js

---
+++ Exercícios 0507(TEM QUE ENVIAR).js
@@ -0,0 +1,43 @@
+//Mostrar os n primeiros números primos(FAIL)
+var n = parseInt(prompt("Digite um número:"));
+var nprim = 0;
+var ndiv = 0;
+var s = "";
+    for(i=1;nprim<=n;i++)
+    {
+        for(pdiv=0;pdiv<=i;pdiv++)
+        {
+            if(i%pdiv==0)
+            {
+                ndiv++;
+            }
+        }
+        if(ndiv == 2)
+        {
+            nprim++;
+            s += i+" ";
+        }
+    }
+alert(s);
+
+//Os divisores de 12 são: 1, 2, 4, 6 e 12. O maior divisor de 12 que não excede a raís quadrada de 12 é 3. Podemos chamar o maior divisor de um inteiro N que não excede a raís quadrada de N, de pseudoraíz quadrada(PRQ) de N, por exemplo: PRQ de 3102 = 47. Faça um programa que que calcule o prq de um número sem usar Nath.sqrt
+var n = parseInt(prompt("Digite um número:"))
+var raiz;
+for(c=1;c<=n;c++)
+{
+    if(n%c==0 && c*c <= n)
+    {
+        raiz = c;
+    }
+}
+alert("A pseudo-raiz quadrada desse número é: "+raiz+".");
+
+//Calcular potenciação sem usar função matemática
+var base = parseInt(prompt("Digite a base da potência:"));
+var ind = parseInt(prompt("Digite o expoente:"));
+var res = 1;
+for(c=0;c<ind;c++)
+{
+    res *= base;
+}
+alert(base+"^("+ind+") = "+res);
