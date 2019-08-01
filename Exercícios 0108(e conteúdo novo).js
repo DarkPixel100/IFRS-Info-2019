877ef5fefbda19d8f0ce413185272f993d901687
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-08-01 14:15:25 -0300

    Update Exercícios 0108(e conteúdo novo).js

--- Exercícios 0108(e conteúdo novo).js
+++ Exercícios 0108(e conteúdo novo).js
@@ -1,12 +1,12 @@
 //"123456789".substr(6) = "6789";
 //"123456789".substr(6,3) = "678";
-                     ^
-                     a partir dessa, n(nesse caso 3) characters a frente
+//                   ^
+//                   a partir dessa, n(nesse caso 3) characters a frente
                      
 //"123456789".substring(6) = "6789";
 //"123456789".substring(6,9) = "678";
-                        ^
-                        a partir dessa até a n(nesse caso 9) posição
+//                      ^
+//                      a partir dessa até a n(nesse caso 9) posição
                         
 //"123456789".replace("789","jooj") = "123456jooj";
 
