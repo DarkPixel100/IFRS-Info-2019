67c61bf85db39c99474d7aa73a2659e0f9bf2791
DarkPixel100 <49082448+DarkPixel100@users.noreply.github.com>
2019-12-13 19:50:37 -0300

    Update Mine_Chunk.js

--- Mine_Chunk.js
+++ Mine_Chunk.js
@@ -1,11 +1,11 @@
 var s = "";
-var c01 = parseInt(prompt("Digite o primeiro valor do chunk inicial:"));
-var c02 = parseInt(prompt("Digite o segundo calor do chunk inicial:"));
-var c11 = parseInt(prompt("Digite o primeiro valor do chunk final:"));
-var c12 = parseInt(prompt("Digite o segundo calor do chunk final:"));
 do
 {
     var vet = [];
+    var c01 = parseInt(prompt("Digite o primeiro valor do chunk inicial:"));
+    var c02 = parseInt(prompt("Digite o segundo calor do chunk inicial:"));
+    var c11 = parseInt(prompt("Digite o primeiro valor do chunk final:"));
+    var c12 = parseInt(prompt("Digite o segundo calor do chunk final:"));
     for(i=Math.min(c01,c11);i<=Math.max(c01,c11);i++)
     {
         for(j=Math.min(c02,c12);j<=Math.max(c02,c12);j++)
